bc39b9890a6c394d08696fff2d1af3033a107ff7
DavidB <davibursonvivero.db@gmail.com>
2024-02-15 15:26:53 +0100

    comit 2

--- src/main/java/com/microcompany/accountsservice/constraints/AccountOwner.java
+++ src/main/java/com/microcompany/accountsservice/constraints/AccountOwner.java
@@ -1,6 +1,8 @@
 package com.microcompany.accountsservice.constraints;
 
 import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,7 +10,18 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = {AccountName.Validator.class})
+@Constraint(validatedBy = {AccountOwner.Validator.class})
 public @interface AccountOwner {
 
+    public class Validator implements ConstraintValidator<AccountOwner, String> {
+        @Override
+        public void initialize(final AccountOwner name) {
+        }
+
+        @Override
+        public boolean isValid(final String s, final ConstraintValidatorContext constraintValidatorContext) {
+            String cleanName = s.trim();
+            return (cleanName.length() >= 3 && cleanName.length() <= 20);
+        }
+    }
 }
